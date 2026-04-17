a71b2272a3cdef21128b7c59d2346cf54012afe0
David Ruales <hruales1138@gmail.com>
2026-04-17 13:40:07 -0500

    commit

--- otro_tema/modelos.py
+++ otro_tema/modelos.py
@@ -1,4 +1,8 @@
 import numpy as np
+import sel as sl
+import sympy as sp
+
+x = sp.symbols('x')
 
 def Matrix(x_data):
     n = len(x_data)
@@ -8,3 +12,9 @@ def Matrix(x_data):
         for i in range(0, n):
             A[i,j] = A[i, j-1]*x_data[i]
     return A
+
+def polinomial_simple(x_data, y_data):
+    A = Matrix(x_data)
+    coeficientes = sl.eliminacion_DD(A, y_data)
+    Polinomio = sum(coeficientes[i] * (x**i) for i in range(len(x_data)))
+    return Polinomio
